b8f05e53f1e9647451f6e91282517a1a8ab5b9c0
huttiepant <64630595+huttiepant@users.noreply.github.com>
2025-03-26 14:02:39 +0400

    Update layout.tsx adding GA-4 tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,28 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="en">
+      <head>
+        {/* GA4 Script */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-5H3Q3JTZWR', {
+                send_page_view: true,
+                allow_ad_personalization_signals: false
+              });
+            `,
+          }}
+        ></script>
+      </head>
       <body>{children}</body>
     </html>
   )
